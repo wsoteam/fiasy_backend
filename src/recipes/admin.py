a418d5e9b4a75c554dab5df6ebdbac80f7ae4332
Mykhailo Istomin <m.istomin@pp.ratiose.com>
2019-07-14 23:06:35 +0300

    filters

--- src/recipes/admin.py
+++ src/recipes/admin.py
@@ -5,6 +5,8 @@ from recipes.models import Recipe
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['name', 'get_ingredients_elements']
+    list_filter = ('products__name',)
+    search_fields = ('name', 'full_info', 'products__name',)
 
 
 admin.site.register(Recipe, RecipeAdmin)
