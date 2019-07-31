f5905759423556e0704adaa812f59b0d461e82a8
Mykhailo Istomin <m.istomin@pp.ratiose.com>
2019-07-31 09:57:18 +0300

    swagger urls

--- src/api/swagger_urls.py
+++ src/api/swagger_urls.py
@@ -6,5 +6,5 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Fiasy API')
 
 urlpatterns = [
-    path('', schema_view)
+    path('', schema_view,)
 ]
