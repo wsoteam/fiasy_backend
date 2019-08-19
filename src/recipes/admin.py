ab5faf7dd4d6ebac443c1e4dbc4cf856af61a0f8
Mykhailo Istomin <m.istomin@pp.ratiose.com>
2019-08-19 10:56:19 +0300

    Recipes search filters TN-7839

--- src/recipes/admin.py
+++ src/recipes/admin.py
@@ -2,10 +2,32 @@ from django.contrib import admin
 
 from recipes.models import Recipe
 
+from products.admin import InputFilter
+
+from django.db.models import Q
+
+from django.utils.translation import ugettext_lazy as _
+
+
+class ProductFilter(InputFilter):
+    parameter_name = 'product'
+    title = _('By Brand')
+
+    def queryset(self, request, queryset):
+        term = self.value()
+        if term is None:
+            return
+        any_name = Q()
+        for bit in term.split():
+            any_name &= (
+                Q(products__name__icontains=bit)
+            )
+        return queryset.filter(any_name)
+
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['name']
-    # list_filter = ('products__name',)
+    list_filter = (ProductFilter,)
     search_fields = ('name', 'full_info', 'products__name',)
 
 
