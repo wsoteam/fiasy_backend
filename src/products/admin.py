6070d02d17c155b10d392a35f6b9e6c3b26bf5a1
Mykhailo Istomin <m.istomin@pp.ratiose.com>
2019-08-19 10:55:22 +0300

    Products search filters TN-7838

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,9 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+from django.db.models import Q
+
 from django.utils.translation import ugettext_lazy as _
 
 from products.models import Product, Brand, Category, MeasurementUnit
+
+
+class InputFilter(admin.SimpleListFilter):
+    template = 'admin/input_filter.html'
+
+    def lookups(self, request, model_admin):
+        # Dummy, required to show the filter.
+        return ((),)
+
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))
+        all_choice['query_parts'] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+        yield all_choice
+
+
+class BrandFilter(InputFilter):
+    parameter_name = 'brand'
+    title = _('By Brand')
+
+    def queryset(self, request, queryset):
+        term = self.value()
+        if term is None:
+            return
+        any_name = Q()
+        for bit in term.split():
+            any_name &= (
+                Q(brand__name__icontains=bit)
+            )
+        return queryset.filter(any_name)
 
 
 class MeasurementUnitInline(admin.TabularInline):
@@ -22,7 +58,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'brand', 'category', 'portion']
-    list_filter = ('brand', 'category',)
+    list_filter = (BrandFilter, 'category')
     search_fields = ('name', 'brand__name',)
     inlines = [
         MeasurementUnitInline
@@ -36,6 +72,12 @@ for category in Category.objects.all():
         change_category,
         name=_('Add to ') + str(category.name)
     )
+
+
+# class TransactionAdmin(admin.ModelAdmin):
+#     list_filters = (
+#         CategoryFilter,
+#     )
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Brand)
