a09c715dc585b510a58853c0c69ff4126618185e
BrainIsDead <pro100Istomin1988@gmail.com>
2019-12-13 14:13:30 +0200

    Recipes rest serializers

--- src/api/serializers/recipes.py
+++ src/api/serializers/recipes.py
@@ -1,9 +1,34 @@
 from rest_framework import serializers
 from rest_framework import filters
 
-from recipes.models import Recipe
+from recipes.models import Recipe, ProductAmount
+from products.models import Product
 
 from api.serializers.products import ProductSerializer
+
+
+class ProductAmountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductAmount
+        fields = ('amount',)
+
+
+class RecipeProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'amount'
+        )
+
+    def to_representation(self, instance):
+            representation = super().to_representation(instance)
+            representation['amount'] = ProductAmountSerializer(
+                instance.amount.all(),
+                many=True
+            ).data
+            return representation
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -13,7 +38,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation['products'] = ProductSerializer(
+        representation['products'] = RecipeProductSerializer(
             instance.products.all(),
             many=True
         ).data
