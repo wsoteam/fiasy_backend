92b8e404c6488c2de57dfeacbdfab4adade05090
BrainIsDead <pro100Istomin1988@gmail.com>
2020-02-18 08:41:38 +0200

    serializers for intermittent_fasting

---
+++ src/api/serializers/intermittent_fasting.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from intermittent_fasting.models import Fasting
+
+
+class FastingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Fasting
+        fields = '__all__'
